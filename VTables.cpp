dcc89a2f344bec6b27089d64d848ad8b5a9c5000
FriendlyFire <jpguertin.cg@gmail.com>
2024-03-15 21:14:13 -0400

    More robust

--- VTables.cpp
+++ VTables.cpp
@@ -1,5 +1,7 @@
 #include "framework.h"
 
+#include <set>
+
 #define CINTERFACE
 #define D3D11_NO_HELPERS
 #include <d3d11.h>
@@ -13,15 +15,24 @@ HRESULT STDMETHODCALLTYPE HkPresent(
 	return RealPresent(This, SyncInterval, Flags);
 }
 
+std::set<IDXGISwapChainVtbl*> g_SwapChainTables;
+std::set<ID3D11DeviceVtbl*> g_DeviceTables;
+std::set<ID3D11DeviceContextVtbl*> g_DeviceContextTables;
+
 void OverwriteVTables(void* sc, void* dev, void* ctx)
 {
 	auto* swapChainVT = reinterpret_cast<IDXGISwapChain*>(sc)->lpVtbl;
 	auto* deviceVT = reinterpret_cast<ID3D11Device*>(dev)->lpVtbl;
 	auto* contextVT = reinterpret_cast<ID3D11DeviceContext*>(ctx)->lpVtbl;
 
-	DWORD oldProtect;
-	VirtualProtect(&swapChainVT->Present, sizeof(void*), PAGE_READWRITE, &oldProtect);
+	if (!g_SwapChainTables.contains(swapChainVT))
+	{
+		DWORD oldProtect;
+		VirtualProtect(&swapChainVT->Present, sizeof(void*), PAGE_READWRITE, &oldProtect);
 
-	RealPresent = swapChainVT->Present;
-	swapChainVT->Present = &HkPresent;
+		RealPresent = swapChainVT->Present;
+		swapChainVT->Present = &HkPresent;
+
+		g_SwapChainTables.insert(swapChainVT);
+	}
 }
